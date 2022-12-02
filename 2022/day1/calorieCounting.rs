96acac32f1220330d5a6c269b9104d2e5dd5a05b
Addie-Finke <addie.finke2@gmail.com>
2022-12-02 13:26:18 -0800

    Pt 2 of calorie counting complete

--- 2022/day1/calorieCounting.rs
+++ 2022/day1/calorieCounting.rs
@@ -1,26 +1,34 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::str;
 
 fn main() -> io::Result<()>{
     let file = File::open("input.txt")?;
     let iter = BufReader::new(file);
 
-    let mut curr_total = 0;
-    let mut curr_max = 0;
+    let mut curr_totals =[0, 0, 0];
+    let mut curr_num = 0;
     for line in iter.lines(){
         let l = line?.clone();
         if l.eq(""){ 
-            if curr_total > curr_max{
-                curr_max = curr_total;
+            if curr_num > curr_totals[0]{
+                curr_totals[2] = curr_totals[1];
+                curr_totals[1] = curr_totals[0];
+                curr_totals[0] = curr_num;
             }
-            curr_total = 0;
+            else if curr_num > curr_totals[1]{
+                curr_totals[2] = curr_totals[1];
+                curr_totals[1] = curr_num;
+            }
+            else if curr_num > curr_totals[2]{
+                curr_totals[2] = curr_num;
+            }
+            curr_num=0;
         }
         else{
-            curr_total += l.parse::<i32>().unwrap();
+            curr_num += l.parse::<i32>().unwrap();
         }
     }
-
-    println!("{}", curr_max);
+    let max = curr_totals[0] + curr_totals[1] + curr_totals[2];
+    println!("{}", max);
     Ok(())
 }
